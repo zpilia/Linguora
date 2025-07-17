ca8cb7ca714c77925f227fab683f04f2c61e24dd
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-07-17 18:56:13 +0200

    feat: create initial iOS project with SwiftUI in Xcode

---
+++ linguora/linguora/linguoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  linguoraApp.swift
+//  linguora
+//
+//  Created by Zoé Pilia on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct linguoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ linguora/linguoraTests/linguoraTests.swift
@@ -0,0 +1,17 @@
+//
+//  linguoraTests.swift
+//  linguoraTests
+//
+//  Created by Zoé Pilia on 17/07/2025.
+//
+
+import Testing
+@testable import linguora
+
+struct linguoraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
