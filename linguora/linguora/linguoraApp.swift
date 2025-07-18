4ca049745a931e1098e5d5e3aac950d33fed18ac
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-07-18 19:03:06 +0200

    feat: set ContentView as home screen with navigation title "Accueil"

--- linguora/linguora/linguoraApp.swift
+++ linguora/linguora/linguoraApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct linguoraApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+                    .navigationTitle("Accueil")
+            }
         }
     }
 }
